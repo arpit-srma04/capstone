cdca2c3868efed9826ef321d91f7cb81c771d0e4
EC2 Default User <ec2-user@ip-172-31-82-56.ec2.internal>
2024-11-15 06:30:30 +0000

    tf_config

--- main.tf
+++ main.tf
@@ -5,21 +5,15 @@ provider "aws" {
 resource "aws_instance" "ec_2" {
   ami           = "ami-063d43db0594b521b"
   instance_type = "t2.micro"
-  
-  monitoring    = true  # Correct way to enable detailed monitoring
 
   tags = {
     Name = "team_1_capstone"
   }
+
+  monitoring = true
 }
 
-# Make the S3 bucket name unique by using random_id
-resource "random_id" "bucket_suffix" {
-  byte_length = 8
-}
-
-resource "aws_s3_bucket" "bucketforcapstone" {
-  bucket = "bucketusingterraform${random_id.bucket_suffix.hex}"  # Make bucket name unique
+resource "aws_s3_bucket" "t1bucket" {
+  bucket = "vedanth-arpit"
   acl    = "private"
 }
-
